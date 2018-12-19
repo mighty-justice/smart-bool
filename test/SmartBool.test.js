69216547c28d124fa549cdae9ca59dd4d5037faf
Robert Kuykendall <robert@rkuykendall.com>
2018-12-19 17:08:09 -0600

    Improve coverage

--- test/SmartBool.test.js
+++ test/SmartBool.test.js
@@ -6,7 +6,47 @@ describe('SmartBool', () => {
     const sb = new SmartBool();
 
     expect(sb.isTrue).toBe(false);
+    expect(sb.isFalse).toBe(!sb.isTrue);
+
     sb.setTrue();
+
     expect(sb.isTrue).toBe(true);
+    expect(sb.isFalse).toBe(!sb.isTrue);
+
+    sb.setFalse();
+
+    expect(sb.isTrue).toBe(false);
+    expect(sb.isFalse).toBe(!sb.isTrue);
+  });
+
+  it('Toggles boolean values', async () => {
+    const sb = new SmartBool();
+
+    expect(sb.isTrue).toBe(false);
+    expect(sb.isFalse).toBe(!sb.isTrue);
+
+    sb.toggle();
+
+    expect(sb.isTrue).toBe(true);
+    expect(sb.isFalse).toBe(!sb.isTrue);
+
+    sb.toggle();
+
+    expect(sb.isTrue).toBe(false);
+    expect(sb.isFalse).toBe(!sb.isTrue);
+  });
+
+  it('Awaits a request', async () => {
+    const sb = new SmartBool();
+
+    const request = new Promise(resolve => setTimeout(resolve, 100));
+    expect(sb.isTrue).toBe(false);
+    expect(sb.saving()).toBe('Save');
+    const sbUntil = sb.until(request);
+    expect(sb.saving()).toBe('Saving...');
+    expect(sb.isTrue).toBe(true);
+    await sbUntil;
+    expect(sb.saving()).toBe('Save');
+    expect(sb.isTrue).toBe(false);
   });
 });
